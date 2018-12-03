8a7851b8fc1fe5348774e7c34833b64ef7fdbaf4
sirfcsn <sirfcsn@gmail.com>
2018-12-04 00:08:22 +0900

    feat: Add store and connect it into Root.js

---
+++ src/store/modules/index.js
@@ -0,0 +1,6 @@
+import { default as base } from './base';
+import { default as editor } from './editor';
+import { default as list } from './list';
+import { default as post } from './post';
+export { penderReducer as pender } from 'redux-pender';
+
