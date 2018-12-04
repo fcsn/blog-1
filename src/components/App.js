ab3d330e1d00d9effe758122203c42d2bddc5f8c
sirfcsn <sirfcsn@gmail.com>
2018-12-04 10:33:03 +0900

    feat: Install node-sass, classnames, open-color, Add Header layout in PageTemplate.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { ListPage, PostPage, NotFoundPage, EditorPage } from 'pages';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => {
     return (
         <div>
-            BlogApp
+            <Switch>
+                <Route exact path="/" component={ListPage}/>
+                <Route path="/page/:page" component={ListPage}/>
+                <Route path="/tag/:tag/:page?" component={ListPage}/>
+                <Route path="/post/:id" component={PostPage}/>
+                <Route path="/editor" component={EditorPage}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
         </div>
     );
 };
